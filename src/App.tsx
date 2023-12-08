6064858aa0cf4bb46b467ae22665fcf7a385fa59
tuanna2 <tuanna2@hblab.vn>
2023-12-08 14:04:02 +0700

    manual calculate

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,20 @@ function App() {
   const [isChangePage, setIsChangePage] = React.useState<boolean>(false);
 
   return isChangePage ? (
-    <>
+    <div
+      style={
+        {
+          "--vh": `${window.innerHeight * 0.01}px`,
+        } as React.CSSProperties
+      }
+      className="container"
+    >
       <div className="box orange">20%</div>
       <div className="box white">40%</div>
       <div className="box orange">60%</div>
       <div className="box white">80%</div>
       <div className="box orange">100%</div>
-    </>
+    </div>
   ) : (
     <div className="over-height">
       <p>Not all browsers support dvh</p>
